9ff8d403b82f26885b53fd08615d8b1123173fe2
Vinoth <r.vinoth@live.com>
2023-10-06 06:29:41 +0200

    Added youtube plugin sample

---
+++ Sample/YouTubeSearch/Program.cs
@@ -0,0 +1,27 @@
+﻿using AzureAI.Community.Microsoft.Semantic.Kernel.PlugIn.Web.YouTube;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Skills.Web;
+
+namespace YouTubeSearchSample
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, Youtube Connector for Microsoft Semantic Kernel");
+
+            var youTubeConnector = new YouTubeConnector("youtube-api-key");
+
+            IKernel kernel = Kernel.Builder.Build();
+            
+            var youtubeSkill = kernel.ImportSkill(new WebSearchEngineSkill(youTubeConnector), nameof(YouTubeConnector));
+
+            var result = await kernel.RunAsync("Bot composer", youtubeSkill["search"]);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
